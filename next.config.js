bc78d86e9486a513b773c132962dc69a98d4e39a
comfortsleepbarbados <comfortsleepbarbados@gmail.com>
2026-02-16 17:59:24 -0400

    Updated next.config.js

---
+++ next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.comfortsleepbarbados.com',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
+
