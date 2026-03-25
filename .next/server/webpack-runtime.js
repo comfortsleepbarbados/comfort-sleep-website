f02249f5789800f75aea725b625dede414eea6ff
comfortsleepbarbados <comfortsleepbarbados@gmail.com>
2026-03-25 18:23:00 -0400

    Updated app-build-manifest.json

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -30,7 +30,7 @@
 /******/ 		// Execute the module function
 /******/ 		var threw = true;
 /******/ 		try {
-/******/ 			__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 			__webpack_modules__[moduleId].call(module.exports, module, module.exports, __webpack_require__);
 /******/ 			threw = false;
 /******/ 		} finally {
 /******/ 			if(threw) delete __webpack_module_cache__[moduleId];
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("206fa27a946c2371")
+/******/ 		__webpack_require__.h = () => ("7548f2ce9e8ed317")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "afadc553536cbaa0"; }
+/******/ 		__webpack_require__.h = function() { return "5bee16f214d28dca"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
